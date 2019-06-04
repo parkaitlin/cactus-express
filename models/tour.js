7cf4447b314d01b77757377ad0366c8f0bf649b3
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-06-04 09:26:30 -0700

    create tour model

--- models/tour.js
+++ models/tour.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const TourSchema = mongoose.Schema({
-    eventId: {type: Number, required: true, unique: true},
+    eventId: {type: Number, required: true},
     venue: {type: String, required: true},
-    startTime: {type: String, required: true},
-
+    startTime: {type: Date},
+    eventDates: [],
+    registeredPlayers: [],
+    // leaderboard: add later will probably be an array of objects
+    teeTimes: [], // array of objects {time: , players: [player1, player2, player3]}
+    totalPurse: String, // $
+    firstPlace: String, // $
+    status: String // active or cancelled
 })
+
+module.exports = mongoose.model('Tour', TourSchema);
