8e127ae4a4ef69c62615585253c14e5c63b6ca3d
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-06-11 09:10:01 -0700

    user upcoming tournaments on profile page

--- controllers/tourC.js
+++ controllers/tourC.js
@@ -34,12 +34,49 @@ module.exports = {
     update: async (req, res)=>{
         try {
             const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {new: true})
-            console.log(updatedTour)
             res.json({
                 status: 200,
                 message: "Tournament has been updated."
             })
             
+        } catch (error) {
+            res.json({
+                error: error
+            })
+        }
+    },
+    playerRegistration: async(req, res)=>{
+        try {
+            const user = await User.findById(req.session.userDbId);
+            const tour = await Tour.findById(req.params.id);
+            user.registeredTours.push(tour._id);
+            user.save();
+            tour.registeredPlayers.push({_id: user._id, firstName: user.firstName, lastName: user.lastName, hometown: user.hometown, state: user.state});
+            tour.save();
+
+            res.json({
+                status: 200,
+                user: user,
+                tour: tour
+            })
+        } catch (error) {
+            res.json({
+                error: error
+            })
+        }
+    },
+    upcomingTours: async(req, res)=>{
+        const allTours = []
+        try {
+            const currentUser = await User.findById(req.session.userDbId)
+            for(let i = 0; i < currentUser.registeredTours.length - 1; i++){
+                const tour = await Tour.findById(currentUser.registeredTours[i])
+                allTours.push(tour)
+            }
+            res.json({
+                status: 200,
+                allTours: allTours
+            })
         } catch (error) {
             res.json({
                 error: error
--- routes/tourR.js
+++ routes/tourR.js
@@ -5,5 +5,7 @@ const controller = require('../controllers/tourC');
 router.post('/new', controller.addTour);
 router.get('/all', controller.index);
 router.put('/:id', controller.update);
+router.get('/registration/:id', controller.playerRegistration);
+router.get('/upcoming', controller.upcomingTours);
 
 module.exports = router;
